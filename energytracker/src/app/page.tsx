76db4f77e2a31bf0c6ff0ab30647f173a77cbf22
gtheox <gteodoro197@gmail.com>
2024-11-22 22:43:23 -0300

    pagina incial

---
+++ energytracker/src/app/page.tsx
@@ -0,0 +1,11 @@
+import News from "../components/Home/Home"
+
+export default function Home() {
+    return (
+        <div className="flex justify-center bg-[#00102c] h-full">
+            <div>
+                <News />
+            </div>
+        </div>
+    )
+}
