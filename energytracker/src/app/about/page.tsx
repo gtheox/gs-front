38f27373823773e4336b9596f7f16a330526b25a
lukashibuya <uraluka@uol.com.br>
2024-11-22 22:48:59 -0300

    pagina about

---
+++ energytracker/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import About from "../../components/About/About";
+
+export default function about() {
+    return (
+        <div className="flex justify-center bg-[#00102c]">
+                <About />
+        </div>
+    );
+}
