b6e127f6beb0546ac770c1df26209587295a2e94
lukashibuya <uraluka@uol.com.br>
2024-11-22 22:49:42 -0300

    pagina entrada

---
+++ energytracker/src/app/entrada/page.tsx
@@ -0,0 +1,12 @@
+import Login from '@/components/Login/Login'
+import News from '../../components/Home/Home'
+
+export default function Entrada() {
+    return (
+        <div className="flex justify-center bg-[#00102c]">
+            <Login />
+            <News />
+        </div>
+    )
+}
+
